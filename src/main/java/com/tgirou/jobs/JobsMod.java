f39785d8c7187b296887d47bb44c5037dbe7f553
tgirou <girou.tom@gmail.com>
2022-09-13 22:19:20 +0200

    [0.0.3-1.18.2] Register first event (Breaking block event)

--- src/main/java/com/tgirou/jobs/JobsMod.java
+++ src/main/java/com/tgirou/jobs/JobsMod.java
@@ -1,20 +1,16 @@
 package com.tgirou.jobs;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.world.level.block.Block;
+import com.tgirou.jobs.events.ForgeBreakEvent;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
-
-import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(JobsMod.MOD_ID)
@@ -25,13 +21,22 @@ public class JobsMod
 
     public static final String MOD_ID = "jobs";
 
+    private static final IEventBus forgeEventBus = MinecraftForge.EVENT_BUS;
+
     public JobsMod()
     {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
 
         // Register ourselves for server and other game events we are interested in
-        MinecraftForge.EVENT_BUS.register(this);
+        forgeEventBus.register(this);
+        forgeEventBus.register(new ForgeBreakEvent());
+        forgeEventBus.addGenericListener(Entity.class, JobsMod::forgeEventHandler);
+    }
+
+    // This event is on the forge bus
+    private static void forgeEventHandler(AttachCapabilitiesEvent<Entity> event) {
+
     }
 
     private void setup(final FMLCommonSetupEvent event) {
---
+++ src/main/java/com/tgirou/jobs/events/ForgeBreakEvent.java
@@ -0,0 +1,22 @@
+package com.tgirou.jobs.events;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.event.world.BlockEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class ForgeBreakEvent {
+    @SubscribeEvent
+    public void registerBreakEvent(BlockEvent.BreakEvent event) {
+        Player player = event.getPlayer();
+        Block block = event.getState().getBlock();
+        boolean didGetEXP = false;
+        final BlockPos pos = event.getPos();
+        System.out.println("BLOCK " + block.getRegistryName() + " destroyed.");
+        didGetEXP = true;
+        if (didGetEXP) {
+            System.out.println("1 XP added to " + player.getName().getContents());
+        }
+    }
+}
